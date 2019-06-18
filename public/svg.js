5ec1cbeca1c4e94f761c98427a5f83eacf9203cf
Justin York <justinyork@familysearch.org>
2019-06-17 21:45:59 -0600

    Show scripture refs in a bottom panel

--- public/svg.js
+++ public/svg.js
@@ -1,6 +1,7 @@
 (function () {
-  // TODO: need to add a simple static server because this doesn't work locally via the files API
-  // Or I force the browser to download it by including via a scrip tag?
+  const refsTitle = document.querySelector('#refs-title');
+  const refsContent = document.querySelector('#refs-content');
+
   const refsPromise = fetch(`${graphName}Refs.json`).then(response => response.json());
   
   Array.from(document.querySelectorAll('#svg g.node')).forEach(group => {
@@ -11,10 +12,7 @@
 
   async function showRefs(sign) {
     const refData = await refsPromise;
-    console.log('sign refs', refData.nodes[sign]);
-
-    // Should I show it in a popup or panel?
-    // What is the mobile experience? 
-    // What is the desktop experience?
+    refsTitle.textContent = sign;
+    refsContent.textContent = refData.nodes[sign].join(', ');
   }
 })();
