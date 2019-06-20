28d11f6a50e0a90fab2231e6df4561a3a37ba9f7
Justin York <justinyork@familysearch.org>
2019-06-19 21:21:03 -0600

    Add pan and zoom

--- public/svg.js
+++ public/svg.js
@@ -1,11 +1,36 @@
 (function () {
+
+  /**
+   * Setup zooming and panning
+   */
+  const svg = document.querySelector('#svg-container svg');
+
+  // Having the width and height on the SVG messes with the zoom and pan
+  // calculations so we remove it and just set it to the container size
+  // via CSS.
+  svg.width = '';
+  svg.height = '';
+  
+  window.svg = svgPanZoom(svg, {
+    minZoom: 1,
+    maxZoom: 6,
+    fit: false,
+    contain: true,
+    controlIconsEnabled: true,
+    // Faster zooming
+    zoomScaleSensitivity: 0.3,
+  });
+
+  /**
+   * Setup scripture references
+   */
+
   const refsTitle = document.querySelector('#refs-title');
   const refsContent = document.querySelector('#refs-content');
-
   const refsPromise = fetch(`${graphName}Refs.json`).then(response => response.json());
   
-  Array.from(document.querySelectorAll('#svg g.node')).forEach(group => {
-    group.addEventListener('click', (event) => {
+  Array.from(document.querySelectorAll('#svg-container g.node')).forEach(group => {
+    group.addEventListener('click', () => {
       showRefs(group.querySelector('text').textContent);
     });
   });
